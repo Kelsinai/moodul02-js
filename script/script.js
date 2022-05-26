554f8dbde702ece4cc124f267ccebe98c41cf67e
Kelsinai Kalykulova <kelsinaikalykulova@gmail.com>
2022-05-26 10:37:48 +0600

    Add JS

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// задание-1
+let user = "Heelo,JS";
+document.write(user);
+
+//задание-2
+let user1 = "Javascript-мультипарадигменный язык программирования";
+document.write(user1);
+
+user1 = "I love Osh ";
+document.write(user1);
+
+//задание-3
+// let name = "kelsinai";
+// document.write(name);
+
+let numer = 3929;
+document.write(number);
